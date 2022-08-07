f05f4e629ccca8ef6f427349ac309848b16f5280
Thomas Stone <tjs030@bucknell.edu>
2022-08-07 12:24:38 -0600

    Initial setup

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            Worldly
+        </div>
+        <div>
+            <p>link 1</p>
+            <p>link 2</p>
+            <p>link 2</p>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
